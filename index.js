72e4a457f1ec80161d3afa48a05ce52e25ef8647
Caitlin Jordan <jordan.cait17@gmail.com>
2023-01-18 07:25:18 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400){
+    return 'This one is on me!';
+  } else if (someNumber <= 2000){
+    return 'That will be twenty bucks.';
+  } else if (someNumber <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (someNumber > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let greeting = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  return greeting;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let greeting;
+  switch (tip){
+    case 'generous':
+      greeting = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      greeting = 'Thank you.';
+      break;
+    default:
+      greeting = 'Bye.';
+      break;
+  }
+  return greeting;
 }
